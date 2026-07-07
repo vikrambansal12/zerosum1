d0e8d3513c3e1fd7ba565a25db04c971fcf35130
Nishit Kumar <nishitpra333@gmail.com>
2026-07-07 17:42:37 +0530

    Make drone-rescue product images fit within the box (object-fit: contain)

--- zerosumtechnologies.com/js/collaboration-products.js
+++ zerosumtechnologies.com/js/collaboration-products.js
@@ -55,7 +55,7 @@
         }).join('') + '</div></div>'
       : '';
 
-    var isContain = (section === 'skypower' || section === 'dynotis' || section === 'uav-navigation');
+    var isContain = (section === 'skypower' || section === 'dynotis' || section === 'uav-navigation' || section === 'drone-rescue');
     var objectClass = isContain ? 'object-contain bg-slate-50' : 'object-cover';
     var objectFit = isContain ? 'contain' : 'cover';
     var imageSrc = p.image ? '../' + p.image : '';
