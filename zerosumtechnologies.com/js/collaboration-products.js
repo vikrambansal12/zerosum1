6ed520e58ae561c6e2e253c0b64f0381b0bfefe7
Nishit Kumar <nishitpra333@gmail.com>
2026-07-07 21:29:25 +0530

    Make Eureka Dynamics product images fit within the box (object-fit: contain)

--- zerosumtechnologies.com/js/collaboration-products.js
+++ zerosumtechnologies.com/js/collaboration-products.js
@@ -55,7 +55,7 @@
         }).join('') + '</div></div>'
       : '';
 
-    var isContain = (section === 'skypower' || section === 'dynotis' || section === 'uav-navigation' || section === 'drone-rescue');
+    var isContain = (section === 'skypower' || section === 'dynotis' || section === 'uav-navigation' || section === 'drone-rescue' || section === 'eureka-dynamics');
     var objectClass = isContain ? 'object-contain bg-slate-50' : 'object-cover';
     var objectFit = isContain ? 'contain' : 'cover';
     var imageSrc = p.image ? '../' + p.image : '';
